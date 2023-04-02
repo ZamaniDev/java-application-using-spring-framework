4c65d4469af7a222f2edeeffac60351696addff5
Arsalan <contactarsiiz@gmail.com>
2023-04-02 23:50:22 +0330

    added first configuration and context

--- src/main/java/com/nxjava/myapp/HelloWorldConfiguration.java
+++ src/main/java/com/nxjava/myapp/HelloWorldConfiguration.java
@@ -3,11 +3,41 @@ package com.nxjava.myapp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person (String name, int age, Address address){};
+record Address (String firstLine, String City){};
+
+
 @Configuration
 public class HelloWorldConfiguration {
 
     @Bean
     public String name(){
-        return "||| ARSLAN ||||";
+        return "ARSLAN";
     }
+    @Bean
+    public int age(){
+        return 22;
+    }
+
+    @Bean
+    public Person person(){
+        return new Person("Amir", 22, new Address("Darya", "Lahijan"));
+    }
+
+    @Bean
+    public Person person2MethodCall(){
+        return new Person(name(),age(), address()); // name, age
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address neshani){
+        return new Person(name, age, neshani); // name, age
+    }
+
+    @Bean(name = "neshani")
+    public Address address(){
+        return new Address("Azadi St.", "Tehran");
+    }
+
 }
+
--- src/main/java/com/nxjava/myapp/MyappApplication.java
+++ src/main/java/com/nxjava/myapp/MyappApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 @SpringBootApplication
 public class MyappApplication {
 
@@ -15,6 +18,14 @@ public class MyappApplication {
 
         // 2: Config things that Spring manages - @Configuration
         System.out.println(context.getBean("name"));
+        System.out.println(context.getBean("age"));
+        System.out.println(context.getBean("person"));
+        System.out.println("person2MethodCall---- "+context.getBean("person2MethodCall"));
+        System.out.println("person3Parameters---- "+context.getBean("person3Parameters"));
+        System.out.println(context.getBean(Address.class));
+        // --------------------------------------------
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
     }
 
 }
